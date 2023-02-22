a6db7eb4471bf38269837f970ef512a9419d066a
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-02-22 16:11:48 +0700

    Initialized

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::{ Write, Read };
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("input.txt");
+    let display = path.display();
+
+    // Create file
+    let mut file = match File::create(&path) {
+        Err(reason) => panic!("couldn't create {}: {}", display, reason),
+        Ok(file) => file,
+    };
+
+    let mut _input = String::new();
+
+    println!("Enter your name: ");
+    std::io::stdin().read_line(&mut _input).expect("input failed");
+
+    file.write_all(&mut _input.as_bytes()).expect("write file");
+
+    // Read text from file
+    let mut file_reader = match File::open(&path) {
+        Err(reason) => panic!("Couldn't open file {}: {}", display, reason),
+        Ok(file) => file
+    };
+
+    let mut str = String::new();
+    file_reader.read_to_string(&mut str).unwrap();
+    println!("Read from file: {}", str);
+
+}
